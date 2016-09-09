26ee3fc64c91d4a74ba0d7fa0bc541b628240ca0
Yoanatan Hengli <yoanatanhengli@admins-mbp-8.mynet>
2016-09-09 23:47:02 +0300

    config and env routes

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
 'use strict';
 
-const route = require('koa-router')();
+const router = require('koa-router')();
 
 const config = require('../config');
 const Webhook = require('../controllers/Webhook');
 
-route.all('/webhook', Webhook.handle);
+router.all('/webhook', Webhook.handle);
 
 router.get('/env', function *() {
     this.body = process.env;
 });
-route.get('/config', function *() {
+router.get('/config', function *() {
     this.body = config;
 });
 
-module.exports = route.routes();
+module.exports = router.routes();
